643178a13056b40cb11f9fc45f4bdb3bdeeb9822
Leo Sato <leo@i544c.me>
2017-08-18 01:15:11 +0900

    インデントを間違ってspace4にしていたので、space2に修正
    やはり一般的にRubyのインデントはspace2なのか。

--- lib/bargai/cli.rb
+++ lib/bargai/cli.rb
@@ -4,28 +4,28 @@ require "open-uri"
 require "nokogiri"
 
 module Bargai
-    class CLI < Thor
-        desc "list [AREA]", "show list of shop where at AREA"
-        def list(area)
-            # 予めスクレイピングするxpathをjsonか何かで集めておいて、それを基にスクレイピングする
-            # {
-            #   "area": "hakodate",
-            #   "list": "//div[@class='info']",
-            #   "next": "paged"
-            # }
+  class CLI < Thor
+    desc "list [AREA]", "show list of shop where at AREA"
+    def list(area)
+      # 予めスクレイピングするxpathをjsonか何かで集めておいて、それを基にスクレイピングする
+      # {
+      #   "area": "hakodate",
+      #   "list": "//div[@class='info']",
+      #   "next": "paged"
+      # }
 
-            url = "http://www.bar-gai.com/?cat=18"
-            charset = nil
-            html = open(url) do |f|
-                charset = f.charset
-                f.read
-            end
-            doc = Nokogiri::HTML.parse(html, nil, charset)
+      url = "http://www.bar-gai.com/?cat=18"
+      charset = nil
+      html = open(url) do |f|
+        charset = f.charset
+        f.read
+      end
+      doc = Nokogiri::HTML.parse(html, nil, charset)
 
-            doc.xpath("//div[@class='info']").each do |info|
-                p info.css("h4").inner_text
-            end
-            # 再帰的に次のページを読みに行く？
-        end
+      doc.xpath("//div[@class='info']").each do |info|
+        p info.css("h4").inner_text
+      end
+      # 再帰的に次のページを読みに行く？
     end
+  end
 end
